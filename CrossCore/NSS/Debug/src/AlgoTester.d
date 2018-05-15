1ef940948915fefa4681044522a861ba480535fe
Stinus Skovgaard <stinus2@gmail.com>
2018-05-15 10:12:47 +0200

    tilføjer ekstra output til algotester

--- CrossCore/NSS/Debug/src/AlgoTester.d
+++ CrossCore/NSS/Debug/src/AlgoTester.d
@@ -1,9 +1,9 @@
 src/AlgoTester.doj: ../src/AlgoTester.cpp
-src/AlgoTester.doj: ../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/stdio.h
-src/AlgoTester.doj: ../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/yvals.h
+src/AlgoTester.doj: ../../../../../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/stdio.h
+src/AlgoTester.doj: ../../../../../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/yvals.h
 src/AlgoTester.doj: ../src/AlgoTester.h
 src/AlgoTester.doj: ../src/Algorithm.h
 src/AlgoTester.doj: ../src/LMSFilter.h
-src/AlgoTester.doj: ../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/stdfix.h
-src/AlgoTester.doj: ../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/sys/stdfix_limits.h
-src/AlgoTester.doj: ../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/sys/stdfix_io.h
+src/AlgoTester.doj: ../../../../../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/stdfix.h
+src/AlgoTester.doj: ../../../../../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/sys/stdfix_limits.h
+src/AlgoTester.doj: ../../../../../../../../../../../Analog\ Devices/CrossCore\ Embedded\ Studio\ 2.7.0/Blackfin/include/sys/stdfix_io.h
--- CrossCore/NSS/Debug/system/startup_ldf/subdir.mk
+++ CrossCore/NSS/Debug/system/startup_ldf/subdir.mk
@@ -33,28 +33,28 @@ C_DEPS += \
 system/startup_ldf/app_cplbtab.doj: ../system/startup_ldf/app_cplbtab.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore Blackfin C/C++ Compiler'
-	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-9cecde0861ec8465294c6fef4f8d494d.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_cplbtab.d" -o "$@" "$<"
+	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-b893300d3ab5a8ae5c8732607184049b.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_cplbtab.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 system/startup_ldf/app_handler_table.doj: ../system/startup_ldf/app_handler_table.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore Blackfin C/C++ Compiler'
-	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-9cecde0861ec8465294c6fef4f8d494d.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_handler_table.d" -o "$@" "$<"
+	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-b893300d3ab5a8ae5c8732607184049b.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_handler_table.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 system/startup_ldf/app_heaptab.doj: ../system/startup_ldf/app_heaptab.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore Blackfin C/C++ Compiler'
-	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-9cecde0861ec8465294c6fef4f8d494d.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_heaptab.d" -o "$@" "$<"
+	ccblkfn.exe -c -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -flags-compiler --no_wrap_diagnostics -si-revision 0.6 -g -DCORE0 -D_DEBUG @includes-b893300d3ab5a8ae5c8732607184049b.txt -structs-do-not-overlap -no-const-strings -no-multiline -warn-protos -D__PROCESSOR_SPEED__=600000000 -double-size-32 -decls-strong -cplbs -gnu-style-dependencies -MD -Mo "system/startup_ldf/app_heaptab.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 system/startup_ldf/app_startup.doj: ../system/startup_ldf/app_startup.s
 	@echo 'Building file: $<'
 	@echo 'Invoking: CrossCore Blackfin Assembler'
-	easmblkfn.exe -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -si-revision 0.6 -g -DCORE0 -D_DEBUG -i"C:\Users\stinu\Desktop\ISB\CrossCore\NSS\system" -gnu-style-dependencies -MM -Mo "system/startup_ldf/app_startup.d" -o "$@" "$<"
+	easmblkfn.exe -file-attr ProjectName="DynFilter" -proc ADSP-BF533 -si-revision 0.6 -g -DCORE0 -D_DEBUG -i"C:\Users\stinu\OneDrive\Desktop\6. Semester\ISB\GIT\ISB\CrossCore\NSS\system" -gnu-style-dependencies -MM -Mo "system/startup_ldf/app_startup.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
